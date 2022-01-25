a97b768c89da9c4dd2f6c5f2190f0f1aca466d4e
pavan-sharma-v <93601797+pavan-sharma-v@users.noreply.github.com>
2022-01-25 16:25:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,15 @@ function autoRefresh() {
     async function getuser() {
       const response = await fetch(api_url);
       const data = await response.json();
+            if((data.country) == "")
+      {
+        document.getElementById("x").innerHTML ="Not Able To Predict Try Different Name";
+      }
+        else{
       document.getElementById("x").innerHTML = data.country[0].country_id;
       document.getElementById("y").innerHTML = data.country[1].country_id;
       document.getElementById("z").innerHTML = data.country[2].country_id;
+        }
     }
     getuser();
   }
